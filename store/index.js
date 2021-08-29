bf0d94ba4104bfd94b4d2423532c63b23b47787d
steve zhang <871506263@qq.com>
2021-08-29 22:35:57 +0800

    提交代码于: 2021-08-29 22:35:57

--- store/index.js
+++ store/index.js
@@ -5,17 +5,20 @@ Vue.use(Vuex);
 const store = new Vuex.Store({
   state: {
     key1: 1,
-    key1: 1,
+    isLogin: false
   },
   getters: {
     key1Getter: state => `hello: ${state.key1}`
   },
   mutations: {
-    setKey1(state, payload) {
-      state.key1 = payload;
+    setKey1(state, val) {
+      state.key1 = val;
     },
-    addKey1(state, payload) {
+    addKey1(state) {
       state.key1 += 1;
+    },
+    login(state) {
+      state.isLogin = true;
     }
   }
 });
